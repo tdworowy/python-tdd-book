4c653f399c3e03925099963cb42dd20eeb4cb3f2
Tomasz <dworowytomasz@gmail.com>
2020-10-06 11:53:01 +0200

    add home page html + tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,19 @@
 from django.test import TestCase
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+from django.urls import resolve
+from lists.views import home_page
+from django.http import HttpRequest
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_html(self):
+        request = HttpRequest
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
