cc0666c30bb338cae623697d251c4c929768bbbb
Tomasz <dworowytomasz@gmail.com>
2020-10-09 10:50:15 +0200

    refactor to use templates

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,8 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
-from django.http import HttpRequest
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_html(self):
-        request = HttpRequest
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/') # use Django test client
+        self.assertTemplateUsed(response, 'home.html')
