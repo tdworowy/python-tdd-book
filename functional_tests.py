7d548300f7fcbb24eaa341ad77bbcfa95c35c7a6
Tomasz <dworowytomasz@gmail.com>
2020-10-09 11:03:16 +0200

    update home.html template

--- functional_tests.py
+++ functional_tests.py
@@ -28,7 +28,7 @@ class NewVisitorTEst(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), 'New to-do item did not appear in table')
 
 
 if __name__ == "__main__":
