bc258f8aadb240861a9d24898ee4eb62bc512427
Tomasz <dworowytomasz@gmail.com>
2020-10-13 13:52:57 +0200

    lots of updates

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,9 +9,10 @@ import unittest
 # TODO need refactor
 class NewVisitorTEst(LiveServerTestCase):
     MAX_WAIT = 10
+    DRIVER_PATH = "driver/geckodriver.exe"
 
     def setUp(self):
-        self.browser = webdriver.Firefox(executable_path="driver/geckodriver.exe")
+        self.browser = webdriver.Firefox(executable_path=self.DRIVER_PATH)
 
     def tearDown(self):
         self.browser.quit()
@@ -29,7 +30,7 @@ class NewVisitorTEst(LiveServerTestCase):
                     raise e
                 time.sleep(0.1)
 
-    def test_can_start_a_list_and_retrieve_it(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
 
@@ -47,3 +48,42 @@ class NewVisitorTEst(LiveServerTestCase):
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+    def test_can_start_a_list_for_multiple_users(self):
+        self.browser.get(self.live_server_url)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        list_url1 = self.browser.current_url
+        self.assertRegex(list_url1, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox(executable_path=self.DRIVER_PATH)
+        self.browser.get(self.live_server_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('1: Buy peacock feathers', page_text)
+        self.assertNotIn('2: Use peacock feathers to make a fly', page_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        list_url2 = self.browser.current_url
+        self.assertRegex(list_url2, '/lists/.+')
+
+        self.assertNotEqual(list_url1,list_url2)
+
--- lists/urls.py
+++ lists/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from lists import views as list_views
-from lists import urls as list_urls
+from django.urls import path
+from lists import views
 
 
 urlpatterns = [
-    path('', list_views.home_page, name='home'),
-    path('lists/', include(list_urls), name="new_list"),
-
+    path('new', views.new_list, name="new_list"),
+    path('<list_id>/', views.view_list, name="view_list"),
+    path('<list_id>/add_item', views.add_item, name="add_item"),
 ]
-
