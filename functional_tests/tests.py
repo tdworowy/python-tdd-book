5cd113de6a5fabd25ab407610da5f13c63468974
Tomasz <dworowytomasz@gmail.com>
2020-10-16 11:33:04 +0200

    fixes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -7,7 +7,7 @@ import unittest
 
 
 # TODO need refactor
-class NewVisitorTEst(LiveServerTestCase):
+class NewVisitorTEst(StaticLiveServerTestCase):
     MAX_WAIT = 10
     DRIVER_PATH = "driver/geckodriver.exe"
 
