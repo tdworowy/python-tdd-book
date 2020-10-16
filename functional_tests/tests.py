d724b037b12a754d7e13c0a4d7fe0fa984f967b0
Tomasz <dworowytomasz@gmail.com>
2020-10-16 10:51:58 +0200

    fist step of FT fro layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -85,5 +85,18 @@ class NewVisitorTEst(LiveServerTestCase):
         list_url2 = self.browser.current_url
         self.assertRegex(list_url2, '/lists/.+')
 
-        self.assertNotEqual(list_url1,list_url2)
+        self.assertNotEqual(list_url1, list_url2)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width'] / 2, 512, delta=10)
+        
+        input_box.send_keys("testing")
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width'] / 2, 512, delta=10)
