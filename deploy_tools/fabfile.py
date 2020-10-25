2e829099a2846339538feaedd9712ca88762c1df
Tomasz <dworowytomasz@gmail.com>
2020-10-25 11:20:42 +0100

    update

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,15 +1,16 @@
-# TODO migrate to fabric 2.0
 import random
-from fabric.contrib.files import append, exists
-from fabric.api import cd, env, local, run
+from fabric import Connection
 
 REPO_URL = 'https://github.com/tdworowy/python-tdd-book.git'
+server = ''
+
+conn = Connection(host=server)
 
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    run(f'mkdir -p {site_folder}')
-    with cd(site_folder):
+    site_folder = f'/home/{conn.env.user}/sites/{conn.env.host}'
+    conn.run(f'mkdir -p {site_folder}')
+    with conn.cd(site_folder):
         _get_latest_source()
         _update_virtualenv()
         _create_or_update_dotenv()
@@ -18,34 +19,38 @@ def deploy():
 
 
 def _get_latest_source():
-    if exists('.git'):
-        run('git fetch')
+    if conn.exists('.git'):
+        conn.run('git fetch')
     else:
-        run(f'git clone {REPO_URL} .')
-    current_commit = local("git log -n 1 --format=%H", capture=True)
-    run(f'git reset --hard {current_commit}')
+        conn.run(f'git clone {REPO_URL} .')
+    current_commit = conn.local("git log -n 1 --format=%H", capture=True)
+    conn.run(f'git reset --hard {current_commit}')
 
 
 def _update_virtualenv():
-    if not exists('virtualenv/bin/pip'):
-        run(f'python3.8 -m venv virtualenv')
-    run('./virtualenv/bin/pip install -r requirements.txt')
+    if not conn.exists('virtualenv/bin/pip'):
+        conn.run(f'python3.8 -m venv virtualenv')
+    conn.run('./virtualenv/bin/pip install -r requirements.txt')
 
 
 def _create_or_update_dotenv():
-    append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME={env.host}')
-    current_contents = run('cat .env')
+    conn.append('.env', 'DJANGO_DEBUG_FALSE=y')
+    conn.append('.env', f'SITENAME={conn.env.host}')
+    current_contents = conn.run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        conn.append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
 
 def _update_static_files():
-    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+    conn.run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 
 def _update_database():
-    run('./virtualenv/bin/python manage.py migrate --noinput')
+    conn.run('./virtualenv/bin/python manage.py migrate --noinput')
+
+
+if __name__ == "__main__":
+    deploy()
