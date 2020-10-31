53d5af775edb4cd39e4acb046bdc0645cac83ff5
Tomasz <dworowytomasz@gmail.com>
2020-10-31 11:48:14 +0100

    make validation work + refactor

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -15,14 +15,14 @@ class NewVisitorTest(FunctionalTest):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box = self.get_item_input_box()
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box = self.get_item_input_box()
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -30,14 +30,14 @@ class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_multiple_users(self):
         self.browser.get(self.live_server_url)
 
-        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box = self.get_item_input_box()
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box = self.get_item_input_box()
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -53,7 +53,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertNotIn('1: Buy peacock feathers', page_text)
         self.assertNotIn('2: Use peacock feathers to make a fly', page_text)
 
-        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box = self.get_item_input_box()
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
         input_box.send_keys('Buy milk')
--- lists/tests/tests_forms.py
+++ lists/tests/tests_forms.py
@@ -1,12 +1,9 @@
 from django.test import TestCase
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.models import List, Item
 
 
 class ItemFormTest(TestCase):
-
-    # def test_from_renders_item_text_input(self):
-    #     form = ItemForm()
-    #     self.fail(form.as_p())
 
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
@@ -21,3 +18,11 @@ class ItemFormTest(TestCase):
         self.assertEqual(
             form.errors['text'], [EMPTY_ITEM_ERROR]
         )
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
